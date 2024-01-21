ad535594e7233e3b53abdec77deffb1cabc6c31e
Nishabora <152774491+Nishabora@users.noreply.github.com>
2024-01-21 22:06:55 +0530

    Update home.js

--- Routes/home.js
+++ Routes/home.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const homeController = require("../Controllers/home.js");
 
+router.get('/',homeController.getUser);
+
+router.get('/searchUser',homeController.getUser)    
+
+router.get('/getOwnerRepos',homeController.getOwnerRepos)
+module.exports = router;
